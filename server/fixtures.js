a00b676a06cd4553b1ec8dbed273e7dff7b55b65
Chad Crawford <ccrawforduwlax@gmail.com>
2015-07-03 01:47:08 -0500

    Add comments collection, pub/sub, and fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,19 +1,73 @@
+// Fixture data
 if (Posts.find().count() === 0) {
-  Posts.insert({
+  var now = new Date().getTime();
+
+  var tomId = Meteor.users.insert({
+    profile: {
+      name: 'Tom Coleman'
+    }
+  });
+  // User userId to grab the user from db
+  // to reference that user in post & comment
+  var tom = Meteor.users.findOne(tomId);
+
+  var sachaId = Meteor.users.insert({
+    profile: {
+      name: 'Sacha Greif'
+    }
+  });
+  var sacha = Meteor.users.findOne(sachaId);
+
+  // Set post name(nameId) eql to insert function
+  // to reference in comment creation
+  var telescopeId = Posts.insert({
     title: 'Introducing Telescope',
-    description: 'This app is for ...and does ...',
-    url: 'http://sachagrief.com/introducing-telescope/'
+    userId: sacha._id,
+    author: sacha.profile.name,
+    url: 'http://sachagreif.com/introducing-telescope/',
+    description: 'Super responsive social platform',
+    submitted: new Date(now - 7 * 3600 * 1000)
   });
 
-  Posts.insert({
+  var meteorId = Posts.insert({
     title: 'Meteor',
-    description: 'This app is great..it does..!',
-    url: 'http://meteor.com'
+    userId: tom._id,
+    author: tom.profile.name,
+    url: 'http://meteor.com',
+    description: 'Awesome full-stack Javscript App platform',
+    submitted: new Date(now - 10 * 3600 * 1000)
   });
 
-  Posts.insert({
+  var meteorBookId = Posts.insert({
     title: 'The Meteor Book',
-    description: 'This thing is unreal..you will love that it...',
-    url: 'http://themeteorbook.com'
+    userId: tom._id,
+    author: tom.profile.name,
+    url: 'http://themeteorbook.com',
+    description: 'Great way to learn to build apps with Meteor',
+    submitted: new Date(now - 12 * 3600 * 1000)
+  });
+
+  Comments.insert({
+    postId: telescopeId,
+    userId: tom._id,
+    author: tom.profile.name,
+    submitted: new Date(now - 5 * 3600 * 1000),
+    body: 'Interesting project Sacha, can I get involved?'
+  });
+
+  Comments.insert({
+    postId: telescopeId,
+    userId: sacha._id,
+    author: sacha.profile.name,
+    submitted: new Date(now - 3 * 3600 * 1000),
+    body: 'You sure can Tom!'
+  });
+
+  Comments.insert({
+    postId: meteorId,
+    userId: sacha._id,
+    author: sacha.profile.name,
+    submitted: new Date(now - 5 * 3600 * 1000),
+    body: 'Awesome project Tom, can I help out?'
   });
 }
