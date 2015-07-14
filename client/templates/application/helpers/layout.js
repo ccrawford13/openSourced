12bcbe49a5f3bace4c05380950fbd67dc17ffbb1
Chad Crawford <ccrawforduwlax@gmail.com>
2015-07-14 01:30:07 -0500

    Add fadeIn/fadeOut page transitions

---
+++ client/templates/application/helpers/layout.js
@@ -0,0 +1,15 @@
+Template.layout.onRendered(function() {
+  this.find('.body__container--inner')._uihooks = {
+    insertElement: function(node, next) {
+      $(node)
+        .hide()
+        .insertBefore(next)
+        .fadeIn();
+    },
+    removeElement: function(node) {
+      $(node).fadeOut(function() {
+        $(this).remove();
+      });
+    }
+  }
+});
