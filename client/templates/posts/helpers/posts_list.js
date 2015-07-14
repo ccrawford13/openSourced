aca8d8bd0b125099825d7bca513288583d8aa078
Chad Crawford <ccrawforduwlax@gmail.com>
2015-07-14 00:46:29 -0500

    Fade items in and out on create and destroy

--- client/templates/posts/helpers/posts_list.js
+++ client/templates/posts/helpers/posts_list.js
@@ -1,5 +1,11 @@
 Template.postsList.onRendered(function() {
   this.find('.post__wrapper')._uihooks = {
+    insertElement: function(node, next) {
+      $(node)
+        .hide()
+        .insertBefore(next)
+        .fadeIn();
+    },
     moveElement: function(node, next) {
       var $node = $(node),
         $next = $(next);
@@ -30,6 +36,11 @@ Template.postsList.onRendered(function() {
       // reset everything to 0, animated
       $node.addClass('animate').css('top', 0);
       $inBetween.addClass('animate').css('top', 0);
+    },
+    removeElement: function(node) {
+      $(node).fadeOut(function() {
+        $(this).remove();
+      });
     }
   }
 });
